5ce329133db7aebc68c42c623158ef398221a353
gatoama <franksoler@gmail.com>
2021-05-18 19:03:07 +0100

    Added parameter and renamed foo.

--- sample.lisp
+++ sample.lisp
@@ -1,5 +1,15 @@
+; Original function foo from
+; https://puntoblogspot.blogspot.com/2018/04/bootstrap-your-emacs-lisp-learning.html
+
 (defun foo ()
   (interactive)
   (save-excursion
     (beginning-of-line)
     (insert ";")))
+
+; Rename foo and add parameter.
+(defun make-comment (comment-str)
+  (interactive "sEnter comment string: ")
+  (save-excursion
+    (beginning-of-line)
+    (insert comment-str)))
