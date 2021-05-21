a439f81b64c48e15c0f45a7e0c6621890b5c8468
gatoama <franksoler@gmail.com>
2021-05-21 18:39:42 +0100

    Added documentation strings and renamed make-comment function to make-comment-line.

--- sample.el
+++ sample.el
@@ -2,13 +2,15 @@
 ; https://puntoblogspot.blogspot.com/2018/04/bootstrap-your-emacs-lisp-learning.html
 
 (defun foo ()
+  "Smallest practical sample code to learn elisp."
   (interactive)
   (save-excursion
     (beginning-of-line)
     (insert ";")))
 
 ; Add parameter and rename foo.
-(defun make-comment (comment-str)
+(defun make-comment-line (comment-str)
+  "Use parameter in function"
   (interactive "sEnter comment string: ")
   (save-excursion
     (beginning-of-line)
