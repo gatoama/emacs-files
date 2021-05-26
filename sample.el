bbe0a875572137f02b0ff3da88e7d0981ec6336e
gatoama <franksoler@gmail.com>
2021-05-27 00:35:00 +0100

    Carry out operation on an entire buffer.

--- sample.el
+++ sample.el
@@ -15,3 +15,25 @@
   (save-excursion
     (beginning-of-line)
     (insert comment-str)))
+
+; Carry out operation on an entire buffer.
+(defun make-comment-buffer (comment-str)
+  "Carry out operation over entire buffer."
+  (interactive "sEnter comment string: ")
+  (save-excursion
+    (beginning-of-buffer)
+    (while (not eobp))
+      (beginning-of-line)
+      (insert comment-str)
+      (forward-line 1)))
+
+(defun my/walk-line-by-line ()
+  "Carry out operation over entire buffer."
+  (interactive)
+  (save-excursion
+    (beginning-of-buffer)
+    (while (not (eobp))
+      (beginning-of-line)
+      (insert comment-str)      
+      (forward-line-1)
+      )))
